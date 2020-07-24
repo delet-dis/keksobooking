a9c9dda3e023c8b4b27c08be68248fa3776dad92
Igor <efimov.036@gmail.com>
2020-07-24 22:38:35 +0700

    added map activetor

---
+++ js/page-activetor.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//объявление переменных
+const mapPinMain = document.querySelector('.map__pin--main'),
+      formFieldsets = document.querySelectorAll('fieldset'),
+      map = document.querySelector('.map');
+
+//отключение форм по дефолту
+formFieldsets.forEach(element => element.disabled = true);
+
+//слушатель перемещения метки
+mapPinMain.addEventListener('mouseup', function(){
+  map.classList.remove('map--faded');
+  formFieldsets.forEach(element => element.disabled = false);
+})
