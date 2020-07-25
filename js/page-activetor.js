94d288baad017b420a1f41ba0b4c396821608f01
Igor <efimov.036@gmail.com>
2020-07-25 14:01:36 +0700

    started calculating position of element

--- js/page-activetor.js
+++ js/page-activetor.js
@@ -3,13 +3,20 @@
 //объявление переменных
 const mapPinMain = document.querySelector('.map__pin--main'),
   formFieldsets = document.querySelectorAll('fieldset'),
-  map = document.querySelector('.map');
+  map = document.querySelector('.map'),
+  addressForm = document.querySelector('input[name=address]');
 
-//отключение форм по дефолту
+//константы
+const MAP_PIN_WIDTH = mapPinMain.offsetWidth;
+const MAP_PIN_HEIGHT = mapPinMain.offsetHeight;
+
+//дефолтные действия
 formFieldsets.forEach(element => element.disabled = true);
 
+
 //слушатель перемещения метки
-mapPinMain.addEventListener('mouseup', function () {
+mapPinMain.addEventListener('mouseup', function (evt) {
   map.classList.remove('map--faded');
   formFieldsets.forEach(element => element.disabled = false);
+  console.log(mapPinMain.offsetParent);
 })
