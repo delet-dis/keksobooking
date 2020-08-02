03da47e1c973ddb30053942273aff11b3547ed96
Igor <efimov.036@gmail.com>
2020-08-02 14:31:08 +0700

    moved code to self-invoking functions

--- js/dragger.js
+++ js/dragger.js
@@ -1,50 +1,51 @@
 'use strict';
+(function () {
+  //слушатель перетаскивания пина
+  mapPinMain.addEventListener('mousedown', (evt) => {
+    evt.preventDefault();
 
-//слушатель перетаскивания пина
-mapPinMain.addEventListener('mousedown', (evt) => {
-  evt.preventDefault();
-
-  let startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-
-  //функция высчитывания координат
-  const onMouseMove = moveEvt => {
-    moveEvt.preventDefault();
-    let shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
+    let startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
     };
 
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
+    //функция высчитывания координат
+    const onMouseMove = moveEvt => {
+      moveEvt.preventDefault();
+      let shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      let styleChange = {
+        top: mapPinMain.offsetTop - shift.y,
+        left: mapPinMain.offsetLeft - shift.x
+      };
+
+      if (styleChange.left > 0 &&
+        styleChange.left < 1200 &&
+        styleChange.top > 110 &&
+        styleChange.top < 660) {
+        mapPinMain.style.top = styleChange.top + 'px';
+        mapPinMain.style.left = styleChange.left + 'px';
+      };
+
     };
 
-    let styleChange = {
-      top: mapPinMain.offsetTop - shift.y,
-      left: mapPinMain.offsetLeft - shift.x
+    //функция отписки от обработчика события
+    const onMouseUp = upEvt => {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
     };
 
-    if (styleChange.left > 0 &&
-      styleChange.left < 1200 &&
-      styleChange.top > 110 &&
-      styleChange.top < 660) {
-      mapPinMain.style.top = styleChange.top + 'px';
-      mapPinMain.style.left = styleChange.left + 'px';
-    };
-
-  };
-
-  //функция отписки от обработчика события
-  const onMouseUp = upEvt => {
-    upEvt.preventDefault();
-
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-  };
-
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-});
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})()
--- js/page-activator.js
+++ js/page-activator.js
@@ -1,71 +1,72 @@
 'use strict';
+(function () {
+  //объявление переменных
+    window.mapPinMain = document.querySelector('.map__pin--main');
+    const formFieldsets = document.querySelectorAll('fieldset'),
+    map = document.querySelector('.map'),
+    addressForm = document.querySelector('input[name=address]'),
+    pins = document.querySelectorAll('.map__pin');
+  //константы
 
-//объявление переменных
-const mapPinMain = document.querySelector('.map__pin--main'),
-  formFieldsets = document.querySelectorAll('fieldset'),
-  map = document.querySelector('.map'),
-  addressForm = document.querySelector('input[name=address]'),
-  pins = document.querySelectorAll('.map__pin');
-//константы
+  //функция получения координат элемента относительно документа
+  function getCoords(elem) {
+    let box = elem.getBoundingClientRect();
 
-//функция получения координат элемента относительно документа
-function getCoords(elem) {
-  let box = elem.getBoundingClientRect();
-
-  return {
-    top: box.top + pageYOffset,
-    left: box.left + pageXOffset
-  };
-}
-
-//функция высчитывания положения метки
-function getPinCoords() {
-  const MAP_PIN_WIDTH = mapPinMain.offsetWidth;
-  const MAP_PIN_HEIGHT = mapPinMain.offsetHeight + 12;
-
-  const MAP_X_COORD = getCoords(map).left;
-  const MAP_Y_COORD = getCoords(map).top;
-
-  const PIN_X_COORD = getCoords(mapPinMain).left;
-  const PIN_Y_COORD = getCoords(mapPinMain).top;
-
-  return {
-    pinX: Math.floor(PIN_X_COORD - MAP_X_COORD + MAP_PIN_WIDTH / 2),
-    pinY: Math.floor(PIN_Y_COORD - MAP_Y_COORD + MAP_PIN_HEIGHT)
-  };
-}
-
-//функция заполнения адреса в зависимости от положения метки
-function inputAddressFiller() {
-  addressForm.value = getPinCoords().pinX.toString() + ', ' + getPinCoords().pinY.toString();
-}
-
-//функция отрисовки пинов
-function displayPins() {
-  for (let i = 0; i < numberOfAds; i++) {
-    createPin(dataResult[i]);
-    createCard(dataResult[i]);
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset
+    };
   }
-  const pins = document.querySelectorAll('.map__pin');
-  const cards = document.querySelectorAll('.map__card');
-  for (let i = 1; i < pins.length; i++) {
-    pins[i].addEventListener('click', function () {
-      cards.forEach((item) => {
-        item.classList.add('hidden');
+
+  //функция высчитывания положения метки
+  function getPinCoords() {
+    const MAP_PIN_WIDTH = mapPinMain.offsetWidth;
+    const MAP_PIN_HEIGHT = mapPinMain.offsetHeight + 12;
+
+    const MAP_X_COORD = getCoords(map).left;
+    const MAP_Y_COORD = getCoords(map).top;
+
+    const PIN_X_COORD = getCoords(mapPinMain).left;
+    const PIN_Y_COORD = getCoords(mapPinMain).top;
+
+    return {
+      pinX: Math.floor(PIN_X_COORD - MAP_X_COORD + MAP_PIN_WIDTH / 2),
+      pinY: Math.floor(PIN_Y_COORD - MAP_Y_COORD + MAP_PIN_HEIGHT)
+    };
+  }
+
+  //функция заполнения адреса в зависимости от положения метки
+  function inputAddressFiller() {
+    addressForm.value = getPinCoords().pinX.toString() + ', ' + getPinCoords().pinY.toString();
+  }
+
+  //функция отрисовки пинов
+  function displayPins() {
+    for (let i = 0; i < numberOfAds; i++) {
+      createPin(window.dataResult[i]);
+      createCard(window.dataResult[i]);
+    }
+    const pins = document.querySelectorAll('.map__pin');
+    const cards = document.querySelectorAll('.map__card');
+    for (let i = 1; i < pins.length; i++) {
+      pins[i].addEventListener('click', function () {
+        cards.forEach((item) => {
+          item.classList.add('hidden');
+        })
+        cards[i - 1].classList.remove('hidden');
       })
-      cards[i - 1].classList.remove('hidden');
-    })
+    }
   }
-}
 
-//дефолтные действия
-formFieldsets.forEach(element => element.disabled = true);
-inputAddressFiller();
-
-//слушатель перемещения метки
-mapPinMain.addEventListener('mouseup', function (evt) {
-  map.classList.remove('map--faded');
-  formFieldsets.forEach(element => element.disabled = false);
+  //дефолтные действия
+  formFieldsets.forEach(element => element.disabled = true);
   inputAddressFiller();
-  displayPins();
-});
+
+  //слушатель перемещения метки
+  mapPinMain.addEventListener('mouseup', function (evt) {
+    map.classList.remove('map--faded');
+    formFieldsets.forEach(element => element.disabled = false);
+    inputAddressFiller();
+    displayPins();
+  });
+})();
