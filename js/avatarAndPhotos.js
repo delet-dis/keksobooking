d9b2d5b717e68bb986cf6e75eb4898cc71218c63
Igor <efimov.036@gmail.com>
2020-08-20 17:23:12 +0300

    added ad photos upload

--- js/avatarAndPhotos.js
+++ js/avatarAndPhotos.js
@@ -1,14 +1,14 @@
 'use strict';
 
 (function () {
-  const filePicker = document.querySelector('.upload input[type=file]'),
+  const adAvatarPicker = document.querySelector('#avatar'),
     preview = document.querySelector('.notice__preview img');
 
   const FILE_TYPES = ['gif', 'jpeg', 'png', 'jpg'];
 
   //слушатель изменения аватарки
-  filePicker.addEventListener('change', () => {
-    let file = filePicker.files[0];
+  adAvatarPicker.addEventListener('change', () => {
+    let file = adAvatarPicker.files[0];
 
     let fileName = file.name.toLowerCase();
 
@@ -26,4 +26,31 @@
       reader.readAsDataURL(file);
     }
   });
+
+  const adImgPicker = document.querySelector('#images'),
+    adImgContainer = document.querySelector('.form__photo-container');
+
+  adImgPicker.addEventListener('change', () => {
+    let file = adImgPicker.files[0];
+
+    let fileName = file.name.toLowerCase();
+
+    let matches = FILE_TYPES.some((item) => {
+      return fileName.endsWith(item);
+    });
+
+    if (matches) {
+      let reader = new FileReader();
+
+      reader.addEventListener('load', () => {
+        let node = document.createElement('img');
+        node.classList.add('ad-form__photo');
+        node.src = reader.result;
+        adImgContainer.insertAdjacentElement('afterbegin', node);
+      });
+
+      reader.readAsDataURL(file);
+    }
+  })
+
 })();
